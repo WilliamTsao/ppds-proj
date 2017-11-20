ba883bbf01b564ca12760302276024cfbb7823f9
William Tsao <williamtsao@Williams-MacBook-Pro-2.local>
2017-11-20 18:55:53 -0500

    init file for db stuff

---
+++ db.py
@@ -0,0 +1,8 @@
+#import MySQLdb as mdb
+
+# con = mdb.connect(host = 'localhost',
+#                   user = 'root',
+#                   database = 'foodie',
+#                   passwd = 'dwdstudent2015',
+#                   charset='utf8', use_unicode=True);
+
