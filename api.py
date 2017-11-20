e8dd4b76793a23ab66fd2a53c863c34e4761564e
William Tsao <williamtsao@Williams-MacBook-Pro-2.local>
2017-11-20 18:53:10 -0500

    Added search api call

---
+++ api.py
@@ -0,0 +1,13 @@
+import requests
+RECIPE_ID = "43ed9009"
+NUTRITION_ID = "cce11d0a"
+RECIPE_API_KEY = "5d20a3cad3a3ca33ed748a1a016b3ad4	—"
+NUTRITION_API_KEY = "a5e95201b7bb5dbbd8c85854076ac38f	—"
+BASE_URL = "https://api.edamam.com/"
+
+def searchFood(data):
+    url = BASE_URL + "search"
+    data["api_id"] = RECIPE_ID
+    data["api_key"] = RECIPE_API_KEY
+    return requests.get(url, params=data).json()
+    pass
