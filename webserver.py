69cd979a2ebc9041a3c51ffe9c3222ab62df4301
William Tsao <williamtsao@Williams-MacBook-Pro-2.local>
2017-11-20 18:58:30 -0500

    built route for /, search, and /login

--- webserver.py
+++ webserver.py
@@ -5,33 +5,33 @@ from datetime import datetime
 import urllib
 app = Flask(__name__)
 
-import MySQLdb as mdb
-
-con = mdb.connect(host = 'localhost', 
-                  user = 'root',
-                  database = 'citibike2',
-                  passwd = 'dwdstudent2015', 
-                  charset='utf8', use_unicode=True);
-
-
+import api
 
 
 @app.route('/')
 def home():
-    return('hi')
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    """process request.params"""
+    searchString = request.args.get('s')
+    data={
+        "q": searchString
+    }
+    """make api call """
+    api_res = api.searchFood(data)
+    """clean up api respond """
+    print(api_res)
+    hits = api_res['hits']
+    """send back respond"""
+    return render_template('search.html', hits=hits)
 
 
-@app.route('/<station_id>')
-def station(station_id):
-    cur=con.cursor(mdb.cursors.DictCursor)
-    cur.execute("SELECT * FROM Docks_Status WHERE station_id="+station_id+" ORDER BY last_communication_time DESC")
-    status = cur.fetchall()
-    cur.close()
-    cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute("SELECT station_name FROM Docks WHERE station_id = "+station_id)
-    name = cur.fetchall()
-    cur.close()
-    return render_template('station_status.html', status= status, name = name[0]['station_name'])
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 
 app.run(host='0.0.0.0', port=8000, debug=True)
